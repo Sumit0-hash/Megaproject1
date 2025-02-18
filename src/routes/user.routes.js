a54cde95d548b7a1d2efc315b063ccc7013e917d
Sumit <sumitbhatia900@gmail.com>
2025-02-18 12:32:27 +0530

    Add new routes for user controller.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,27 +1,46 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvtar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import {refreshAccessToken} from "../controllers/user.controller.js"
+import { refreshAccessToken } from "../controllers/user.controller.js";
 
-const  router=Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([  //handles multiple form fields.
-        {
-            name:"avtar",
-            maxCount:1
-        },{
-            name:"coverImage",
-            maxCount:1
-        }
-    ]),
-    registerUser
-)
-router.route("/login").post(loginUser)
+  upload.fields([
+    //handles multiple form fields.
+    {
+      name: "avtar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
-
-export default router
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails); //patch is important because post can update all details that's not neccessary.
+router.route("/avtar").patch(verifyJWT, upload.single("avtar"), updateUserAvtar);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile);
+router.route("/history").get(verifyJWT,getWatchHistory)
+export default router;
